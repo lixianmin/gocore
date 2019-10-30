fdbb44344e4123e6b471c0e61f5b1082644ca8d0
lixianmin <lixianmin@xatom.com>
2019-10-30 12:24:22 +0800

    在Int32与Int64中加入AddIf方法

--- loom/Int64.go
+++ loom/Int64.go
@@ -17,3 +17,19 @@ func (iam *Int64) Store(value int64) {
 func (iam *Int64) Load() int64 {
 	return atomic.LoadInt64((*int64)(iam))
 }
+
+func (iam *Int64) AddIf(delta int64, predicate func(old int64) bool) bool {
+	var expect, update int64
+	for {
+		expect = atomic.LoadInt64((*int64)(iam))
+		if !predicate(expect) {
+			return false
+		}
+
+		update = expect + delta
+
+		if atomic.CompareAndSwapInt64((*int64)(iam), expect, update) {
+			return true
+		}
+	}
+}
